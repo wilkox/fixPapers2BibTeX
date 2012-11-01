07281f6ca6b84a8aaba0332880d2a7cef39f3dba
David Wilkins <david@wilkox.org>
2012-11-01 11:00:52 +1100

    Replacement working

--- fixPapers2BibTeX.pl
+++ fixPapers2BibTeX.pl
@@ -3,7 +3,7 @@
 $USAGE = q/USAGE:
 
   -h <filename> HTML file, produced by Papers2 exporting a Reference List in IEEE style
-  -b <filename> BibTeX file you wish to repair \emph tags for
+  -b <filename> BibTeX file you wish to repair tags for
 /;
 
 use Getopt::Long;
@@ -55,3 +55,34 @@ while (my $line = <HTML>) {
 }
 
 close HTML;
+
+#fix BibTeX file
+die ("ERROR - could not open BibTeX file $BibTeXFile") unless open(BIBTEX, "<$BibTeXFile");
+my $outputFile = $BibTeXFile . ".fixed";
+die ("ERROR - could not write to output file $outputFile") unless open(OUT, ">$outputFile");
+
+while (my $line = <BIBTEX>) {
+
+  if ($line =~ /^title/) {
+    die "ERROR - could not parse BibTeX title line - are there line breaks in the title? [$line]" unless $line =~ /^title\s=\s\{\{(.+)\}\},\n/;
+    my $title = $1;
+
+    #skip unless the title was stored with a substitute
+    unless (exists $title{$title}) {
+      print OUT $line;
+      next;
+    }
+
+    #replace the title with the substitute
+    print "\nReplacing:\n==\n\t$title\n==\nwith:\n==\n\t$title{$title}\n==\n";
+    $line =~ s/$title/$title{$title}/;
+    print OUT $line;
+
+  } else {
+    print OUT $line;
+  }
+  
+}
+
+close BIBTEX;
+close OUT;
